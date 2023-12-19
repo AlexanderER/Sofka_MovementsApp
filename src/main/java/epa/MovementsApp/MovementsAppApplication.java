823d77f3bb1089ddb98895f6e4baf6eee02c5f8d
AlexanderER <alexer1515@gmail.com>
2023-12-19 14:11:06 -0600

    Initial commit

---
+++ src/main/java/epa/MovementsApp/MovementsAppApplication.java
@@ -0,0 +1,13 @@
+package epa.MovementsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epa/MovementsApp/MovementsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package epa.MovementsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
